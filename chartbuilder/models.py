8abdfd86ee756d741ca105664d4bf1a8e8607702
Stefan Wehrmeyer <mail@stefanwehrmeyer.com>
2017-02-16 16:02:40 +0100

    Add max_length argument to overwrite file storage

--- chartbuilder/models.py
+++ chartbuilder/models.py
@@ -18,13 +18,11 @@ from jsonfield import JSONField
 
 
 class OverwriteStorage(FileSystemStorage):
-    '''
-    Muda o comportamento padrão do Django e o faz sobrescrever arquivos de
-    mesmo nome que foram carregados pelo usuário ao invés de renomeá-los.
-    '''
-    def get_available_name(self, name):
+    def get_available_name(self, name, max_length=None):
+        # FIXME: max_length is ignored
         if self.exists(name):
-            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+            full_path = self.path(name)
+            os.remove(full_path)
         return name
 
 
